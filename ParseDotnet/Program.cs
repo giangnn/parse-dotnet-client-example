1358e21d371d49f0a866c1776c1cfe89fe559b22
David Nguyen <david.nguyen@leap.com.au>
2017-02-04 14:55:46 +1100

    initial commit

--- ParseDotnet/Program.cs
+++ ParseDotnet/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿using Parse;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ParseDotnet
 {
     class Program
     {
+        private static Random rand = new Random();
+
         static void Main(string[] args)
         {
+            ParseClient.Initialize(new ParseClient.Configuration
+                {
+                    ApplicationId = "myAppId",
+                    Server = "http://localhost:1337/parse/", // trailing slash is important!
+                WindowsKey = "myDotnetKey"
+                });
+
+            CreateOrder(1, 1);
+            CreateOrder(2, 2);
+
+            Console.ReadLine();
+        }
+
+        static void CreateOrder(int userId, int min)
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var order = new ParseObject("Order");
+                    order["quantity"] = rand.NextDouble() + min;
+                    order["userId"] = userId;
+                    await order.SaveAsync();
+                    Thread.Sleep(1000);
+                }
+            });
         }
     }
 }
